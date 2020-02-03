015178ca715440759d479dae8dcbaa56a7b659e2
Roberto <fig18306@uvg.edu.gt>
2020-02-03 12:54:38 -0600

    Fixed some bugs and add matrix control

--- src/js/client.js
+++ src/js/client.js
@@ -1,20 +1,21 @@
 // coordinate init 
 //TODO
-const blackInitialValues = [{4:4},{3:3}];
-const whiteInitialValues = [{4:3},{3:4}];
 
 function sayHello(elm) {
     return 1;
 };
 
-const coordinates = [];
-let xIndex = 0;
-for(var i = 0; i<64; i++){
-    if(i % 8 === 0 & i != 0){
-        xIndex += 1;
-    }
-    coordinates.push({x:xIndex, y:i%8});
-}
+const coordinates = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,1,-1,0,0,0],
+    [0,0,0,-1,1,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0]
+];
+
 
 
 const root = document.getElementById('root');
@@ -27,34 +28,41 @@ board.style.flexWrap = 'wrap';
 board.style.backgroundColor = 'black';
 root.appendChild(board);
 
-const renderCoin = (color) => {
+const renderCoin = (colorValue) => {
     const coin = document.createElement('div');
     coin.style.width = "100px";
     coin.style.height = "100px";
     coin.style.borderRadius = "50px";   
-    coin.style.backgroundColor = color;
+    coin.style.backgroundColor = colorValue===1 ? 'white' : colorValue===-1 ? 'black' : 'gray';
     coin.style.opacity = '1';
     return coin;
 };  
 
-const renderCell = ({x , y }) => {
+const renderCell = ({
+    x,
+    y,
+    yVal : colorState
+    }) => {
     const cell = document.createElement('div');
     cell.style.width = "100px";
     cell.style.height = "100px";
-    //TODO ADD LODASH COMPARISON FOR SET THE INITIAL COINS
     cell.style.backgroundColor = "gray";
     cell.style.border = '1px solid black';
     cell.className = `${x}-${y}`;
-    
+    console.log(colorState);
+    cell.appendChild(renderCoin(colorState));
     cell.onclick = (elm) => {
-        elm.target.appendChild(renderCoin('red'));  
+        elm.target.appendChild(renderCoin(1));  
     };
     return cell;
 
 };
 
-coordinates.map((val) => {
-    board.appendChild(renderCell(val));
+coordinates.map((xVal,i) => {
+    xVal.map((yVal,j) => {
+        board.appendChild(renderCell({i,j,yVal}));
+          
+    });
 });
 
 
